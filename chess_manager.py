3f334cbaac15046e9f187c0c6ee12144857f23ee
jaredpincus <jpincus@stevens.edu>
2022-11-12 16:30:59 -0500

    Checkmate, etc. functions

--- chess_manager.py
+++ chess_manager.py
@@ -1,11 +1,16 @@
 # https://github.com/zhelyabuzhsky/stockfish
 
-from stockfish import Stockfish
+from stockfish import Stockfish, StockfishException
 import fen_parser
+from enum import Enum
+from typing import Tuple
+import chess
 
-STARTING_BOARD = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+STARTING_BOARD = chess.STARTING_FEN
 
 # TODO stockfish parameters, e.g. depth, threads, ELO rating
+
+# TODO use 'chess.Move' object to translate between stockfish format and standard format
 
 Fen = str  # Chessboard state
 
@@ -33,17 +38,37 @@ def make_ai_move(state:Fen) -> Fen:
     move = sf.get_best_move()
 
     if move == None:
-        # TODO checkmate
-        return state
+        raise StockfishException()
     
     sf.make_moves_from_current_position([move])
     return sf.get_fen_position()
 
 
+def get_legal_moves(state:Fen):
+    board = chess.Board(fen = state)
+    return board.legal_moves
+
+
+def is_check(state:Fen):
+    return chess.Board(fen=state).is_check()
+
+def is_checkmate(state:Fen):
+    return chess.Board(fen=state).is_checkmate()
+
+def is_stalemate(state:Fen):
+    return chess.Board(fen=state).is_stalemate()
+
+
 if __name__ == '__main__':
-    state = STARTING_BOARD
-    print(fen_parser.fen_to_ascii(state) + '\n')
-    state = make_user_move(state, 'e2e4')
-    print(fen_parser.fen_to_ascii(state) + '\n')
-    state = make_ai_move(state)
-    print(fen_parser.fen_to_ascii(state) + '\n')
+    b = chess.Board(fen=STARTING_BOARD)
+    moves = list(map(b.lan, b.generate_legal_moves()))
+    print(moves)
+
+    make_user_move(STARTING_BOARD, 'b5c6')
+
+    #state = STARTING_BOARD
+    #print(fen_parser.fen_to_ascii(state) + '\n')
+    #state,_ = make_user_move(state, 'e2e4')
+    #print(fen_parser.fen_to_ascii(state) + '\n')
+    #state,_ = make_ai_move(state)
+    #print(fen_parser.fen_to_ascii(state) + '\n')
