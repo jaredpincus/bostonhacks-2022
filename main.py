a78a54f356e6704b9acb075faa03414d14f853ac
Alex Miller <98389659+alexjmiller5@users.noreply.github.com>
2022-11-12 16:48:28 -0500

    The full logic of the texting has been implemented in main.py

--- main.py
+++ main.py
@@ -3,177 +3,105 @@ from twilio.rest import Client
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
-account_sid = 'ACe0a977fae42fe85c925bb4bd3eeaf0f1' # replace later with os.environ[]
-auth_token = 'REDACTED' # replace later with os.environ[]
-client = Client(account_sid, auth_token)
+# imported local files
+import chess_manager as cm
+import fen_parser as fp
+import query as q
 
-message = client.messages.create(
-    body='Hi there',
-    from_='+14793244060',
-    to='+12032166345'
-)
+# account_sid = 'ACe0a977fae42fe85c925bb4bd3eeaf0f1' # replace later with os.environ[]
+# auth_token = 'REDACTED' # replace later with os.environ[]
+# client = Client(account_sid, auth_token)
 
-# starting chess board
-start_board = "" 
+STARTING_BOARD = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" # starting chess board
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
+
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
     
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None) 
-    client_phone = request.values.get('from', None)
+    user_phone = request.values.get('from', None)
 
     # Start our TwiML response
     resp = MessagingResponse() 
 
-    # check database to see if there is already a game going
-    if client_phone in database:
+    # check database to see if client has been here before
+    if q.check_user_exists == False:
+        q.create_user(user_phone)
+    
+    if body == "Let's play chess!":
+        resp.message("Black or White?")
+        q.update_board(user_phone, "Game Initiated")
 
-        # retrieve current board state from database
+    user_board = q.get_board_for(user_phone)
 
-        if client_board isn't empty:
-
-            user_move = body
-
-            # retrieve valid moves from stockfish
-            valid_moves = ...
-
-            if user_move in valid_moves:
-                # find new board state given the user's move
-                new_board = move(client_board, user_move)
-
-                # get stockfish's move and all the user's new current possible moves
-                valid_next_moves = ...
-                board_reply = ... # board after stockfish's move
-                
-                
-                # send stockfish's move to the user along with all their current possible next moves
-                resp.message(board_reply)
-                resp.message(valid_next_moves)
-
-                # update database with new current board state
-            else:
-                resp.message("Invalid move, please send a valid move")
-        elif body == "Let's play chess!":
-            resp.message("Black or White?")
-        elif body == "Black":
-            # Send stockfish the starting board with them as the white player and receive their first move
-            # Get valid user moves from stockfish from the starting board
-            valid_user_moves = ...
-            resp.message("Here's my first move! Good luck!\n" + valid_user_moves)
+    if user_board == "Game Initiated":
+        if body == "Black":
+            resp.message("Easy, medium, or hard difficulty?")
+            q.update_board(user_phone, "Game Initiated Black")
         elif body == "White":
-            # Get valid user moves from stockfish from the starting board
-            valid_user_moves = ...
-            resp.message("Your move first! Good luck!\n" + valid_user_moves)
-            
-            
+            resp.message("Easy, medium, or hard difficulty?")
+            q.update_board(user_phone, "Game Initiated White")
+        else:
+            resp.message("Please choose a valid color")
+    elif user_board == "Game Initiated Black":
+            valid_user_moves = cm.get_legal_moves(new_user_board)
+            new_user_board = STARTING_BOARD
+            q.update_board(user_phone, new_user_board)
+            new_user_board = fp.fen_to_ascii(new_user_board)
+
+            resp.message("Your move first! Good luck!\n" + new_user_board + "\nValid Moves:" + valid_user_moves)
+    elif user_board == "Game Initiated White":
+            new_user_board = STARTING_BOARD
+            new_user_board = cm.make_ai_move(new_user_board)
+            q.update_board(user_phone, new_user_board)
+
+            new_user_board = fen_to_ascii(new_user_board)
+            valid_user_moves = cm.get_legal_moves(new_user_board)
+
+            resp.message("Here's my first move! Good luck!\n" + new_user_board + "\nValid Moves:" + valid_user_moves)
+    elif user_board != "":
+        user_move = body
+        prev_valid_moves = cm.get_legal_moves(user_board)
+        if user_move in prev_valid_moves:
+            user_moved_board = cm.make_user_move(user_board, user_move)
+            if cm.is_checkmate(user_moved_board) == True:
+                num_wins = q.get_win(user_phone) + 1
+                q.update_board(user_phone, "")
+                q.update_draw(user_phone, num_wins)
+                resp.message("Congradulations! You won! You have {num_wins} wins total".format)
+            elif cm.is_stalemate(user_moved_board) == True:
+                num_draws = q.get_draw(user_phone) + 1
+                q.update_board(user_phone, "")
+                q.update_draw(user_phone, num_draws)
+                resp.message("Oh no! A stalemate has been reached. You have {num_draws} total".format)
+            else:
+                new_user_board = cm.make_ai_move(user_moved_board)
+                if cm.is_checkmate(new_user_board) == True:
+                    num_losses = q.get_loss(user_phone) + 1
+                    q.update_board(user_phone, "")
+                    q.update_loss(user_phone, num_losses)
+                    resp.message("Oh no! You lost. You have {num_losses} losses total".format)
+                elif cm.is_stalemate(new_user_board) == True:
+                    num_draws = q.get_draw(user_phone) + 1
+                    q.update_board(user_phone, "")
+                    q.update_draw(user_phone, num_draws)
+                    resp.message("Oh no! A stalemate has been reached. You have {num_draws} total".format)
+                else: 
+                    q.update_board(user_phone, new_user_board)
+                    new_user_board = fp.fen_to_ascii(new_user_board)
+                    valid_next_moves = cm.get_legal_moves(new_user_board)
+                    if cm.is_check(new_user_board) == True:
+                        resp.message(new_user_board + "\nYou're in Check!\nValid Moves:" + valid_next_moves)
+                    else:
+                        resp.message(new_user_board + "\nValid Moves:" + valid_next_moves)
+        else:
+            resp.message("Invalid move, please send a valid move")
+
     return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###############################################################################################################################
-# TWILIO EXAMPLE CODE
-###############################################################################################################################
-
-###############################################################################################################################
-# SEND SMS MESSAGES
-###############################################################################################################################
-
-# from flask import Flask, request, redirect
-# from twilio.twiml.messaging_response import MessagingResponse
-
-# app = Flask(__name__)
-
-# @app.route("/sms", methods=['GET', 'POST'])
-# def incoming_sms():
-#     """Send a dynamic reply to an incoming text message"""
-#     # Get the message the user sent our Twilio number
-#     body = request.values.get('Body', None)
-
-#     # Start our TwiML response
-#     resp = MessagingResponse()
-
-#     # Determine the right reply for this message
-#     if body == 'hello':
-#         resp.message("Hi!")
-#     elif body == 'bye':
-#         resp.message("Goodbye")
-
-#     return str(resp)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-###############################################################################################################################
-# SEND MMS MESSAGES
-###############################################################################################################################
-
-# from flask import Flask
-# from twilio.twiml.messaging_response import MessagingResponse
-
-# app = Flask(__name__)
-
-
-# @app.route("/sms", methods=['GET', 'POST'])
-# def sms_reply():
-#     """Respond to incoming calls with a MMS message."""
-#     # Start our TwiML response
-#     resp = MessagingResponse()
-
-#     # Add a text message
-#     msg = resp.message("The Robots are coming! Head for the hills!")
-
-#     # Add a picture message
-#     msg.media(
-#         "https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg"
-#     )
-
-#     return str(resp)
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-###############################################################################################################################
-# SEND BASIC MESSAGES
-###############################################################################################################################
-
-# from flask import Flask, request, redirect
-# from twilio.twiml.messaging_response import MessagingResponse
-
-# app = Flask(__name__)
-
-# @app.route("/sms", methods=['GET', 'POST'])
-# def sms_reply():
-#     """Respond to incoming calls with a simple text message."""
-#     # Start our TwiML response
-#     resp = MessagingResponse()
-
-#     # Add a message
-#     resp.message("The Robots are coming! Head for the hills!")
-
-#     return str(resp)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
