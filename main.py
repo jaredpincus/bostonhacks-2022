013cdd2fde0f693a3e3f89db1d2a4f5da859bf1f
Alex Miller <98389659+alexjmiller5@users.noreply.github.com>
2022-11-12 14:36:27 -0500

    Continuing with the logic of Twilio

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-# # Download the helper library from https://www.twilio.com/docs/python/install
-# import os
-# from twilio.rest import Client
-
-# # Find your Account SID and Auth Token in Account Info and set the environment variables.
-# # See http://twil.io/secure
-# account_sid = 'ACe0a977fae42fe85c925bb4bd3eeaf0f1' # replace later with os.environ[]
-# auth_token = 'REDACTED' # replace later with os.environ[]
-# client = Client(account_sid, auth_token)
-
-# message = client.messages.create(
-#     body='Hi there',
-#     from_='+14793244060',
-#     to='+12032166345'
-# )
-
-# print(message.sid)
-
 import os
 from twilio.rest import Client
-
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -49,19 +30,63 @@ def incoming_sms():
     resp = MessagingResponse() 
 
     # check database to see if there is already a game going
-    if client_phone in databse :
+    if client_phone in database:
 
-    elif 
-    # Determine the right reply for this message
-    if body == "Let's play chess!":
-        resp.message(start_board)
-    elif:
+        # retrieve current board state from database
 
+        if client_board isn't empty:
 
+            user_move = body
+
+            # retrieve valid moves from stockfish
+            valid_moves = ...
+
+            if user_move in valid_moves:
+                # find new board state given the user's move
+                new_board = move(client_board, user_move)
+
+                # get stockfish's move and all the user's new current possible moves
+                valid_next_moves = ...
+                board_reply = ... # board after stockfish's move
+                
+                
+                # send stockfish's move to the user along with all their current possible next moves
+                resp.message(board_reply)
+                resp.message(valid_next_moves)
+
+                # update database with new current board state
+            else:
+                resp.message("Invalid move, please send a valid move")
+        elif body == "Let's play chess!":
+            resp.message("Black or White?")
+        elif body == "Black":
+            # Send stockfish the starting board with them as the white player and receive their first move
+            # Get valid user moves from stockfish from the starting board
+            valid_user_moves = ...
+            resp.message("Here's my first move! Good luck!\n" + valid_user_moves)
+        elif body == "White":
+            # Get valid user moves from stockfish from the starting board
+            valid_user_moves = ...
+            resp.message("Your move first! Good luck!\n" + valid_user_moves)
+            
+            
     return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
